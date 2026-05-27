ec542b6e635ba1cbd9a06cafc6d54cbbc5f31ef1
Tejas Anandas <tejas.anandas@nmiet.edu.in>
2026-05-27 22:06:59 +0530

    Implement removeElements to remove nodes by value
    # LeetCode 203 - Remove Linked List Elements
    
    ## Time Complexity
    
    * Traversal of linked list: O(N)
    * Recursive calls visit each node once: O(N)
    
    Final Time Complexity:
    O(N)
    
    Where:
    N = Number of nodes in the linked list
    
    ---
    
    ## Space Complexity
    
    * Recursive stack space: O(N)
    
    Because recursion stores function calls in the stack.
    
    ---
    
    # Important Concepts Revised
    
    ## Linked List Concepts
    
    * Linked List Traversal
    * Node Connection Manipulation
    * Skipping Nodes
    * Head Node Deletion
    * Recursive Linked List Processing
    
    ---
    
    # Important Recursive Pattern Learned
    
    ```java
    head.next = removeElements(head.next, val);
    ```
    
    Meaning:
    
    * First solve smaller linked list
    * Then reconnect current node
    
    ---
    
    # Important Pointer Manipulation
    
    ```java
    temp.next = temp.next.next;
    ```
    
    Meaning:
    
    * Skip unwanted node
    * Reconnect previous node to next valid node
    
    ---
    
    # Edge Cases Revised
    
    * Empty Linked List
    * Single Node List
    * All Nodes Same As Target
    * Head Node Removal
    * Consecutive Duplicate Target Values
    
    ---
    
    # Important Things To Revise Later
    
    ## Linked List Basics
    
    * Traversal
    * Insertion
    * Deletion
    * Reversal
    * Fast & Slow Pointer
    
    ## Recursive Thinking
    
    * Base Case
    * Recursive Call
    * Returning Modified Structure
    * Backtracking Flow
    
    ## Pointer Visualization
    
    Always visualize:
    
    ```text
    current -> next -> nextnext
    ```
    
    ---
    
    # Main Learning Outcome
    
    This problem teaches:
    
    * How recursive linked list modification works
    * How links are reconnected
    * How returning nodes controls final linked list structure
    * Difference between moving pointers and changing connections

---
+++ removeNode.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public ListNode removeElements(ListNode head, int val) 
+    {
+        if(head==null)
+        {
+            return null;
+        }
+        
+        head.next=removeElements(head.next,val);
+
+        if(head.val==val)
+        {
+            return head.next;
+        }
+
+        return head;
+    }
+}
